3cb0b503f9932bd6eef99f73585278531ea98fc0
ocoleman <owencoleman@outlook.ie>
2020-02-03 15:42:00 +0000

    Week 4 files

---
+++ divides.py
@@ -0,0 +1,13 @@
+#Owen Coleman
+#Check if one number divides another.
+
+p = 8
+m = 3
+
+if p % m == 0:
+    print(p, "divided by", m,"leaves a remainder of zero")
+
+else:
+    print(p, "divided by", m,"does not leave a remainder of zero")
+
+print("test")
