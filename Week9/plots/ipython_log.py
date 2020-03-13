0d113e78151e452576530b694a7c2f9abfe4fb81
ocoleman <owencoleman@outlook.ie>
2020-03-13 17:09:07 +0000

    Week 9 work

--- Week9/plots/ipython_log.py
+++ Week9/plots/ipython_log.py
@@ -1,52 +1,15 @@
 # IPython log file
 
 get_ipython().run_line_magic('logstart', '')
-a = 1
-b = 4
-c = -2
-import numpy as np
+import pandas as pd
+pd.read_csv("http://www.biostat.jhsph.edu/~rpeng/useRbook/faithful.csv")
+df = pd.read_csv("http://www.biostat.jhsph.edu/~rpeng/useRbook/faithful.csv")
+df
+df["waiting"]
 import matplotlib.pyplot as plt
-l = np.arange(0.0, 10.0, 0.5) 
-l
-m = [1,2,3]
-l - l
-l**2
-l*l
-plt.plot(l,l**2)
-plt.show()
-plt.plot(l,l**2)
-plt.show()
-plt.plot(l,l**2,'.')
-plt.show()
-plt.plot(l,l**2,'.')
-x = np.arange(1.0, 10.0, 1.0)
-y1 = x**2
-y2 = 2**x
-y1
-y2
-plt.plot(x, y1)
-plt.plot(x, y1)
-plt.plot(x, y2)
-x = np.linspace(1.0, 10.0, 1000)
-x
-y1 = x**2
-y2 = 2**x
-plt.plot(x, y1, 'g.', label="Squared")
-x = np.linspace(1.0, 10.0, 100)
-y1 = x**2
-y2 = 2**x
-plt.plot(x, y1, 'g.', label="Squared")
-plt.plot(x, y2, 'r.', label="Exponential")
-plt.legend()
-plt.title("Hello, world")
-plt.xlabel("Hello, x")
-plt.ylabel("Hello, y")
-x = np.linspace(1.0, 100.0, 100)
-x
-y1 = x**2
-y2 = 2**x
-plt.plot(x, y1, 'g.', label="Squared")
-plt.plot(x, y2, 'r.', label="Exponential")
-plt.plot(x, np.log(y1), 'g.', label="Squared")
-plt.plot(x, np.log(y2), 'r.', label="Squared")
+plt.plot(df["eruptions"], df["waiting"], 'b.')
+plt.hist(df["eruptions"])
+plt.savefig("hist.png")
+plt.hist(df["eruptions"])
+plt.savefig("hist.png")
 exit()
